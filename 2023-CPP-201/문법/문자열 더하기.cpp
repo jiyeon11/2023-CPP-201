c6d496d2f733433583917c9974e6f9589a4d4316
leejiyeon <s2230@e-mirim.hs.kr>
2023-03-23 11:03:37 +0900

    C스타일 strcat() 문자열 더하기

--- 2023-CPP-201/문법/문자열 더하기.cpp
+++ 2023-CPP-201/문법/문자열 더하기.cpp
@@ -10,12 +10,8 @@ int main(void)
 	char str1[30] = "Jiwoo";
 	char str2[30] = "Rock";
 
-	const int str1_l = strlen(str1);
-	const int str2_l = strlen(str2);
+	strcat(str1, str2);
 
-	for (int i = str1_l; i < str1_l + str2_l; i++) {
-		str1[i] = str2[i - str1_l];
-	}
 	printf("%s \n", str1);
 	return 0;
 }
