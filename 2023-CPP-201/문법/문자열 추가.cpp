81376589bd1168caf37f03665fd0cdb3f9488c8f
leejiyeon <s2230@e-mirim.hs.kr>
2023-03-23 10:48:13 +0900

    C스타일 strlen() 문자열 길이

--- 2023-CPP-201/문법/문자열 추가.cpp
+++ 2023-CPP-201/문법/문자열 추가.cpp
@@ -4,11 +4,6 @@
 int main(void) 
 {
 	char str[30] = "Jiwoo";
-	int size = 0;
-
-	for (int i = 0; str[i] != '\0'; i++) {
-		size++;
-	}
-	printf("문자의 길이는 %d \n", size);
+	printf("문자의 길이는 %d \n", strlen(str));
 	return 0;
 }
