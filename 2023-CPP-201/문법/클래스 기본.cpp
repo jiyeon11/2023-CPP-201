97e00a4d2ddcb8c56ac654a439b966d97b0025d8
leejiyeon <s2230@e-mirim.hs.kr>
2023-04-04 09:12:17 +0900

    매개변수가 없는 생성자를 만들어 보았습니다.

--- 2023-CPP-201/문법/클래스 기본.cpp
+++ 2023-CPP-201/문법/클래스 기본.cpp
@@ -7,22 +7,17 @@ using namespace std;
 //구조체, 클래스는 단어의 첫 글자를 대문자로 한다.
 //class는 디폴트가 private (struct는 디폴트가 public)
 class Student {
-private:
-	string name;
-	//성능 때문에 문자열로 하지 않음.
-	//일반적으로 문자열은 정수 데이터보다 많은 메모리 공간을 요구하며
-	//정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
-	int hakbun;
-	//가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
-	//0:남자, 1:여자
-	int sex;  
-	int age;
-	string department;
-
+public:
 	//생성자(constructor) : 객체가 생성될 때 호출되는 함수
 	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다. 
-	//Student(){}
-public:
+	Student() {
+		name = "연지이";
+		hakbun = 2123;
+		age = 18;
+		sex = 1;
+		department = "뉴미디어 소프트웨어과";
+	}
+
 	//class는 멤버변수를 가질 수 있다.
 	void print(void) {
 		cout << "이름 : " << name << endl;
@@ -31,19 +26,28 @@ public:
 		cout << "성별(0)남자,(1)여자 : " << sex << endl;
 		cout << "학과 : " << department << endl;
 	}
+
+private:
+	string name;
+	//성능 때문에 문자열로 하지 않음.
+	//일반적으로 문자열은 정수 데이터보다 많은 메모리 공간을 요구하며
+	//정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
+	int hakbun;
+	//가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
+	//0:남자, 1:여자
+	int sex;
+	int age;
+	string department;
+
 };
 
-int main(void) 
-{
-	Student stu;
-	stu.name = "연지이";
-	
-	stu.hakbun = 2123;
-	stu.age = 18;
-	stu.sex = 1;
-	stu.department = "뉴미디어 소프트웨어과";
-	
-	stu.print();
-	
-	return 0;
-}
+
+
+	int main(void)
+	{
+		Student stu = Student();
+
+		stu.print();
+
+		return 0;
+	}
