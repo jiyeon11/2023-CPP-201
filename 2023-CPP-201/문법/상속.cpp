bb3a59d2cba6c482c2cbef110294ab93aa80202a
leejiyeon <s2230@e-mirim.hs.kr>
2023-06-15 10:49:20 +0900

    소멸자 앞에는 무조건 virtual

--- 2023-CPP-201/문법/상속.cpp
+++ 2023-CPP-201/문법/상속.cpp
@@ -13,7 +13,7 @@ public:
 		cout << "Animal 생성자()" << endl;
 	}
 
-	~Animal() { cout << "Animal 소멸자()" << endl; }
+	virtual ~Animal() { cout << "Animal 소멸자()" << endl; }
 	//가상함수 : Java처럼 동적바인딩
 	virtual void Roar(void) { cout << "Animal 짖다" << endl; }
 	void Eat(void) { cout << "Animal 먹다" << endl; }
@@ -32,7 +32,7 @@ public:
 	{ 
 		cout << "Rabbit 생성자()" << endl; 
 	}
-	~Rabbit() { cout << "Rabbit 소멸자()" << endl; }
+	virtual ~Rabbit() { cout << "Rabbit 소멸자()" << endl; }
 	//함수 override
 	//다형성(polymorphism) : 시그니쳐(반환형, 이름, 매개변수가 모두 같은)가 같은 함수임에도 다르게 실행되는 것
 	//override : 부모 함수 시그니쳐가 존재하지 않으면 에러발생(실수방지)
