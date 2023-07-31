af7d928b8ad2c9257bb40916d7d844a21c8595a1
jiyeon11 <leejiyeon415@gmail.com>
2023-08-01 08:54:23 +0900

    [상속] 상속을 위한 새로운 프로젝트

---
+++ Inheritance/Inheritance/main.cpp
@@ -0,0 +1,20 @@
+﻿#include<SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main(void)
+{
+	RenderWindow window(VideoMode(1000, 800), "Sangsok");
+
+	While(window.isOpen()) {
+		Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
