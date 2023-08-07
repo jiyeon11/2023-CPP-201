dbce39dc1788ace102e419d8f27d65ca68a02c3e
jiyeon11 <leejiyeon415@gmail.com>
2023-08-08 08:50:59 +0900

    [상속] 객체 delete 처리

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -17,10 +17,8 @@ public:
 	{
 		sprite_->move(x, y);
 	}
-
 	// 실제로 사용하는 것은 Player, Enemy에서 사용
 	virtual void eat(Entity* e) {}
-
 	// getter
 	int get_life() { return life_; }
 	int get_speed() { return speed_; }
@@ -34,14 +32,12 @@ public:
 	int speed_;
 	RectangleShape* sprite_;
 };
-
 class Player : public Entity {
 public:
 	Player(int life, int speed, RectangleShape* sprite, int score)
 		: Entity(life, speed, sprite), score_(score)
 	{
 	}
-
 	// 상대방의 life를 1 깎고, 점수를 획득
 	void eat(Entity* e) override
 	{
@@ -51,7 +47,6 @@ public:
 private:
 	int score_;
 };
-
 int main(void)
 {
 	srand((unsigned int)time(NULL));
@@ -73,11 +68,7 @@ int main(void)
 	se3.setPosition(rand() % 800, rand() % 600);
 	se3.setSize(Vector2f(20, 20));
 	se3.setFillColor(Color::Color(42, 188, 100));
-
-
-	Entity* player = new Entity(3, 5, &sp1);
 	Player* player = new Player(3, 5, &sp1, 0);
-
 	Entity* enemy1 = new Entity(1, 3, &se1);
 	Entity* enemy2 = new Entity(1, 2, &se2);
 	Entity* enemy3 = new Entity(1, 4, &se3);
@@ -87,7 +78,9 @@ int main(void)
 		while (window.pollEvent(e))
 		{
 			if (e.type == Event::Closed)
-				window.close();
+				if (e.type == Event::Closed) {
+					window.close();
+				}
 		}
 		// Input
 		// 방향키 이동
@@ -103,9 +96,7 @@ int main(void)
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			player->move(-player->get_speed(), 0);
 		}
-
 		// Update
-
 		// player가 enemy1과 닿았을 때,
 		if (player->get_sprite().getGlobalBounds().intersects(enemy1->get_sprite().getGlobalBounds()))
 		{
@@ -119,13 +110,8 @@ int main(void)
 		{
 			player->eat(enemy3);
 		}
-
 		// Render
 		window.clear();
-
-		window.draw(enemy1->get_sprite());
-		window.draw(enemy2->get_sprite());
-		window.draw(enemy3->get_sprite());
 		if (enemy1->get_life() > 0) {
 			window.draw(enemy1->get_sprite());
 		}
@@ -135,10 +121,14 @@ int main(void)
 		if (enemy3->get_life() > 0) {
 			window.draw(enemy3->get_sprite());
 		}
-
 		window.draw(player->get_sprite());
 
 		window.display();
 	}
+
+	delete player;
+	delete enemy1;
+	delete enemy2;
+	delete enemy3;
 	return 0;
 }
