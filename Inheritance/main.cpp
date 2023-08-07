de9e2a3ae16303eadeb209ac86fdb951bf85973d
jiyeon11 <leejiyeon415@gmail.com>
2023-08-08 08:42:08 +0900

    [상속] 적을 더 만들어 보았다

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,10 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <stdlib.h>
 #include <time.h>
-
-
 using namespace sf;
-
 class Entity {
 public:
 	// 거의 99.99%이상 클래스를 매개변수로 할 때는 주소값으로 받자
@@ -33,26 +30,35 @@ public:
 	int speed_;
 	RectangleShape* sprite_;
 };
-
 int main(void)
 {
 	srand((unsigned int)time(NULL));
-
 	RenderWindow window(VideoMode(1000, 800), "Sangsok");
 	window.setFramerateLimit(60);
-
 	RectangleShape sp1;
 	sp1.setPosition(400, 300);
 	sp1.setSize(Vector2f(50, 50));
 	sp1.setFillColor(Color::Blue);
-
 	RectangleShape se1;
 	se1.setPosition(rand() % 800, rand() % 600);
 	se1.setSize(Vector2f(40, 40));
 	se1.setFillColor(Color::Yellow);
 
+	RectangleShape se2;
+	se2.setPosition(rand() % 800, rand() % 600);
+	se2.setSize(Vector2f(60, 60));
+	se2.setFillColor(Color::Color(128, 60, 100));
+
+	RectangleShape se3;
+	se3.setPosition(rand() % 800, rand() % 600);
+	se3.setSize(Vector2f(20, 20));
+	se3.setFillColor(Color::Color(42, 188, 100));
+
+
 	Entity* player = new Entity(3, 5, &sp1);
 	Entity* enemy1 = new Entity(1, 3, &se1);
+	Entity* enemy2 = new Entity(1, 2, &se2);
+	Entity* enemy3 = new Entity(1, 4, &se3);
 
 	while (window.isOpen())
 	{
@@ -81,6 +87,9 @@ int main(void)
 		window.clear();
 
 		window.draw(enemy1->get_sprite());
+		window.draw(enemy2->get_sprite());
+		window.draw(enemy3->get_sprite());
+
 		window.draw(player->get_sprite());
 
 		window.display();
